ced3b7e2bb0e94abdf241fcce5371bdada4cb469
OChebanenko <acebanenco@gmail.com>
2021-02-22 09:18:23 +0100

    Experiment with population formula, Xn+1=P*Xn*(1-Xn)

--- src/main/java/com/github/acebanenco/HexagonLifeModel.java
+++ src/main/java/com/github/acebanenco/HexagonLifeModel.java
@@ -15,10 +15,19 @@ public class HexagonLifeModel {
 
     BitSet getNextFill(int WIDTH, int HEIGHT) {
         BitSet nextFill = BitSet.valueOf(currentFill.toLongArray());
+        int totalPoulation = WIDTH * HEIGHT / 2;
+
+        int population = nextFill.cardinality();
+        double xp = population / (double) totalPoulation;
+        double r = 15;
+        double xp1 = r * xp * (1 - xp);
+        int maxNewPopulation = (int) (xp1 * totalPoulation);
+        System.out.printf("Current population is %s%%, next population can be %s%%%n", population*100/totalPoulation, maxNewPopulation*100/totalPoulation);
+
         for (int yn = 0; yn < HEIGHT; yn++) {
             for (int xn = 0; xn < WIDTH; xn++) {
                 int aliveNearbyNeighbours = getNeighbourCount(xn, yn, WIDTH, HEIGHT); // up to 6
-                int aliveSecondRingNeighbours = getFarNeighbours(xn, yn, WIDTH, HEIGHT)/2; // up to 14
+                int aliveSecondRingNeighbours = getFarNeighbours(xn, yn, WIDTH, HEIGHT) / 2; // up to 14
                 int totalNeighbours = aliveNearbyNeighbours;//(aliveNearbyNeighbours + aliveSecondRingNeighbours) / 2; // average
 
                 int currentIndex = xn + yn * WIDTH;
@@ -32,7 +41,20 @@ public class HexagonLifeModel {
                 }
             }
         }
-        if ( nextFill.equals(currentFill) ) {
+        int newPopulation = nextFill.cardinality();
+        if (newPopulation > maxNewPopulation) {
+            double scaleFactor = maxNewPopulation / (double) newPopulation;
+            System.out.printf("Leaving %s%% of population%n", 100 * maxNewPopulation / (newPopulation));
+            IntStream.range(0, WIDTH * HEIGHT)
+                    .filter(nextFill::get)
+                    .forEach(index -> {
+                        if ( random.nextDouble() > scaleFactor ) {
+                            nextFill.set(index, false);
+                        }
+                    });
+        }
+
+        if (nextFill.equals(currentFill)) {
             return null;
         }
         return nextFill;
@@ -76,19 +98,21 @@ public class HexagonLifeModel {
         byte[] bits = new byte[6];
         random.nextBytes(bits);
         int[] neighbours = {
-                bits[0] < -80 ? -1 : reflect(xn + 0,      WIDTH) + WIDTH * reflect(yn - 2, HEIGHT),
-                bits[1] < -80 ? -1 : reflect(xn + 0,      WIDTH) + WIDTH * reflect(yn + 2, HEIGHT),
-                bits[2] < -80 ? -1 : reflect(xn + 1 + dx, WIDTH) + WIDTH * reflect(yn - 1, HEIGHT),
-                bits[3] < -80 ? -1 : reflect(xn + 0 + dx, WIDTH) + WIDTH * reflect(yn - 1, HEIGHT),
-                bits[4] < -80 ? -1 : reflect(xn + 1 + dx, WIDTH) + WIDTH * reflect(yn + 1, HEIGHT),
-                bits[5] < -80 ? -1 : reflect(xn + 0 + dx, WIDTH) + WIDTH * reflect(yn + 1, HEIGHT),
+                bits[0] < -128 ? -1 : reflect(xn + 0, WIDTH) + WIDTH * reflect(yn - 2, HEIGHT),
+                bits[1] < -128 ? -1 : reflect(xn + 0, WIDTH) + WIDTH * reflect(yn + 2, HEIGHT),
+                bits[2] < -128 ? -1 : reflect(xn + 1 + dx, WIDTH) + WIDTH * reflect(yn - 1, HEIGHT),
+                bits[3] < -128 ? -1 : reflect(xn + 0 + dx, WIDTH) + WIDTH * reflect(yn - 1, HEIGHT),
+                bits[4] < -128 ? -1 : reflect(xn + 1 + dx, WIDTH) + WIDTH * reflect(yn + 1, HEIGHT),
+                bits[5] < -128 ? -1 : reflect(xn + 0 + dx, WIDTH) + WIDTH * reflect(yn + 1, HEIGHT),
         };
         int currentIndex = xn + yn * WIDTH;
-        return (int) IntStream.of(neighbours)
-                .filter(i -> i>= 0 && i != currentIndex)
+        int neighboursCount = (int) IntStream.of(neighbours)
+                .filter(i -> i >= 0 && i != currentIndex)
                 .distinct()
                 .filter(currentFill::get)
                 .count();
+        int mutation = 0;//Math.max(random.nextInt(1000+4)-1000, 0);
+        return (neighboursCount < 2 || neighboursCount > 4) && mutation != 0 ? mutation : neighboursCount;
     }
 
     void clear() {
@@ -109,11 +133,11 @@ public class HexagonLifeModel {
     }
 
     private static int reflect(int i, int n) {
-        if ( i < 0 ) {
+        if (i < 0) {
             return -i;
         }
-        if ( i >= n ) {
-            return 2*n - i;
+        if (i >= n) {
+            return 2 * n - i;
         }
         return i;
     }
